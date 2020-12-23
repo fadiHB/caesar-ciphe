660c066107006948ae68a32239edb54e45dc9f35
fadiHB <eng.fadi.hb@gmail.com>
2020-12-23 19:46:39 +0200

    done

--- cryptography/caesar_cipher.py
+++ cryptography/caesar_cipher.py
@@ -1,8 +1,10 @@
 import nltk
 nltk.download('words')
 
-all_letters =list(map(chr, range(97, 123)))
-ALPHABET = ''.join(all_letters).upper() #ABCDEFGHIJKLMNOPQRSTUVWXYZ
+english_words = nltk.corpus.words.words()
+
+# all_letters =list(map(chr, range(97, 123)))
+# ALPHABET = ''.join(all_letters).upper() #ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 
 def encrypt(text,key):
@@ -23,20 +25,47 @@ def encrypt(text,key):
             encrypted += char
     return encrypted
 
-
 def decrypt(text,key):
     return encrypt(text,-1*key)
 
+def find_key (encrypted):
+    results = {}
+    for i in range(26):
+        sentence = decrypt(encrypted,i)
+        matched = 0
+        sentence_words = sentence.split()
+        for word in sentence_words:
+            if word in english_words:
+                matched += 1
+        results[f'{i}'] = matched
+    max_key = max(results, key=results.get)
+    return int(max_key)
+
 
 if __name__ == "__main__":
-    all_letters =list(map(chr, range(97, 123)))
-    st = ''.join(all_letters)
+    # all_letters =list(map(chr, range(97, 123)))
+    # st = ''.join(all_letters)
 
-    encrypted = encrypt('abcd yz',2)
+    encrypted = encrypt('I am eating banana',7)
     print(encrypted)
+
+    print(find_key(encrypted))
 
     decrypted = decrypt(encrypted,2)
     print(decrypted)
+
+
+    # print(english_words)
+    
+
+    # for i in range(3):
+    #     print(i)
+
+    # dic = {}
+    # dic['key'] = 'value'
+    # print(dic)
+
+
   
     # print(ord('a'))
     # print(ord('A'))
--- tests/test_cryptography.py
+++ tests/test_cryptography.py
@@ -1,10 +1,9 @@
 from cryptography import __version__
-from cryptography.caesar_cipher import encrypt,decrypt
+from cryptography.caesar_cipher import encrypt,decrypt,find_key
 
 
 def test_version():
     assert __version__ == '0.1.0'
-
 
 def test_encrypt():
     actual = encrypt('abcd yz',2)
@@ -14,4 +13,10 @@ def test_encrypt():
 def test_decrypt():
     actual = decrypt('cdef ab',2)
     expected = 'abcd yz'
+    assert actual == expected
+
+def test_find_key():
+    encrypted = encrypt('I am eating banana',7)
+    actual = find_key(encrypted)
+    expected = 7
     assert actual == expected
